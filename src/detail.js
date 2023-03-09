83b0b0016fa606038c9dc2e5c31aeaa00e41083c
AntoCast18 <101275905+AntoCast18@users.noreply.github.com>
2023-03-09 13:59:43 -0300

    details, checkbox index

--- src/detail.js
+++ src/detail.js
@@ -1,30 +1,33 @@
 /* Obtenenos los datos de los parametros de la URL */
+
 let params = new URLSearchParams(document.location.search)
 let id = params.get("id")
+ let html = "";
 
-/* Buscar dato por id */
-let evento = data.events.find(events => events._id == id);
+let evento = data.events.find(events => events._id == id);//busca los datos por id
 
-/* Renderizar profile */
-const cardEvents= document.getElementById(".cardevents");
-let html = "";
-html =  `
-    <div class="card2">
-    <div class="card mb-3" style="max-width: 540px;">
-        <div class="row g-0">
-            <div class="col-md-4">
-                <img src="${evento.image}" class="img-fluid rounded-start" alt="foto de ${evento.nombre}">
-            </div>
-            <div class="col-md-8">
-                <div class="card-body">
-                    <h5 class="card-title">${evento.name} ${evento.date}</h5>
-                    <p class="card-text">${evento.description}</p>
-                    <p class="card-text"><small class="text-muted">${evento.price}</small></p>
-                    <p class="card-text">${evento.category} ${evento.capacity} ${evento.assistanse}</p>
-                    <a href="/index.html"class="btn btn-outline-success">VOLVER</a>
+const cardEvents = document.getElementById(".cardevents");/* Renderizar profile */   
+function detalle(event){    
+    html.innerHTML =`
+        <div class="card2">
+            <div class="card mb-3" style="max-width: 540px;">
+                <div class="row g-0">
+                    <div class="col-md-4">
+                    <img src="${evento.image}" class="img-fluid rounded-start" alt="foto de ${evento.nombre}">
+                    </div>
+                    <div class="col-md-8">
+                        <div class="card-body">
+                            <h5 class="card-title">${evento.name} ${evento.date}</h5>
+                            <p class="card-text">${evento.description}</p>
+                            <p class="card-text"><small class="text-muted">${evento.price}</small></p>
+                            <p class="card-text">${evento.category} ${evento.capacity} ${evento.assistanse}</p>
+                            <a href="/index.html" class="btn btn-outline-success">VOLVER</a>
+                        </div>
+                    </div>
                 </div>
             </div>
-        </div>
-    </div>
-</div> `
-cardEvents.innerHTML = html
+        </div>`
+    
+}
+console.log(evento)
+detalle();
--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 const cardEvents = document.querySelector(".cardevents");
 const events = data.events;
+
 let eventosHtml = "";
 function mostrarEvents(events){
     for (const event of events){
@@ -19,3 +20,107 @@ function mostrarEvents(events){
 }
 mostrarEvents(events) 
 cardEvents.innerHTML= eventosHtml;
+
+let arrayCheck = []; //  checkbox seleccionados
+let search = ''; 
+
+
+function categories() {//  para crear checkbox por cada categoria
+    let checkbox = document.getElementById('checkboxes-container');
+    let templateCheckbox = '';
+    let categories = data.events.map(event => event.category);
+    let setCategories = new Set(categories);
+    let arrayCategories = [...setCategories];
+
+    arrayCategories.forEach((category, id) => {
+        templateCheckbox += `   <div class="form-check form-check-inline"> 
+                            <input class="form-check-input" type="checkbox" id="${id}" value="${category}">
+                            <label class="form-check-label" for="${id}">${category}</label>
+                        </div>
+                    `;
+        checkbox.innerHTML = templateCheckbox;
+    })
+}
+categories();
+
+function checkbox() {
+    let checkboxes = document.querySelectorAll('input[type="checkbox"]');
+    
+    checkboxes.forEach(checkbox => {
+        checkbox.addEventListener('change', (box) => {
+            if (box.target.arrayCheck) {
+                arrayCheck.push(box.target.value);
+            } else {
+                arrayCheck = arrayCheck.filter(uncheck => uncheck != box.target.value);
+            }
+            checksAndSearch();
+        });
+    });
+}
+checkbox();
+
+
+function searchCat() {// INPUT
+    let input = document.getElementById('search');
+    input.addEventListener('keyup', (event) => {
+        search = event.target.value;
+        checksAndSearch();
+    });
+}
+searchCat();
+
+
+function checksAndSearch() {// BUSCAR
+    let filteredEvents = [];
+
+    if (arrayCheck.length > 0 && search !== "") {
+        arrayCheck.map(cat => {
+            filteredEvents.push(...data.events.filter(event => event.name.toLowerCase().includes(search.trim().toLowerCase()) &&
+                event.category == cat))
+        })
+
+        console.log(filteredEvents)
+    } else if (arrayCheck.length > 0 && search === "") {
+        arrayCheck.map(cat => {
+            filteredEvents.push(...data.events.filter(event => event.category == cat))
+        })
+    } else if (arrayCheck.length == 0 && search !== "") {
+        filteredEvents.push(...data.events.filter(event => event.name.toLowerCase().includes(search.trim().toLowerCase())))
+    } else {
+        filteredEvents.push(...data.events);
+    }
+    mostrarEvents(filteredEvents);
+}
+
+checksAndSearch();
+
+/* const categoryContainer = document.querySelector(".category");
+const arrayCategories = data.events;
+let= categ = "";
+
+function createCategories(arrayCategories){
+    let id = 1;
+    arrayCategories.forEach(cate => {
+        categ += `
+        <input type"checkbox" class="checkbox" id="categ${id}" value="${cate}">
+        <label for="categ${id}"> ${cate}</label> `
+    })
+    id ++
+}
+
+createCategories(filterCategory)
+categoryContainer.innerHtml = categ; 
+ */
+
+/* const checkboxCategory = document.querySelectorAll('input[type="checkbox"]');
+    
+    for (const checkbox of checkboxCategory) {
+      checkbox.addEventListener('change', () => {
+        const checkedCheckboxes = document.querySelectorAll('input[type="checkbox"]:checked');
+        const values = Array.from(checkedCheckboxes).map(checkbox => checkbox.value);
+        
+        console.log(values);
+      });
+    }
+ */
+ 
